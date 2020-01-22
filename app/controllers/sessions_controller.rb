53f0c5ac4de957523438b3e7c26b1bfc5257d430
Daniel Chincoya <chincoya@ciencias.unam.mx>
2020-01-22 16:29:19 -0600

    Implement login and session

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    puts params[:session][:name]
+    user = User.find_by(name: params[:session][:name])
+    if user
+      session[:id] = user.id
+      redirect_to user
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    session.delete(:id)
+    redirect_to login_path
+  end
+end
